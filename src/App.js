857ce8f06594e6d17ec3193e30e7fc0e24c23f13
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-08 12:11:58 -0400

    finished adding controlled components

--- src/App.js
+++ src/App.js
@@ -154,21 +154,39 @@ function FormAddFriends({ show, onSetFriends }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  const paidByFriend = bill ? bill - paidByUser : "";
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>Bill Value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(+e.target.value)}
+      />
 
       <label>Your share</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(+e.target.value > bill ? paidByUser : +e.target.value)
+        }
+      />
 
-      <label>X's share</label>
-      <input type="text" disabled />
+      <label>{selectedFriend.name}'s share</label>
+      <input type="text" disabled value={paidByFriend} />
 
       <label>Who is paying the bill?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
